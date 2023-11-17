8315244c90e33bce6e1325ccdd38a40df3125c72
Kevin Löhmann <{ID}+{username}@users.noreply.github.com>
2023-11-17 15:20:15 +0100

    added bool randomizer

--- src/testutils/randomize.py
+++ src/testutils/randomize.py
@@ -7,3 +7,6 @@ def random_string(length):
 def random_iterator(length, elem_creator, *args):
     return iter([ elem_creator(*args) for i in range(length) ] )
 
+def random_bool():
+    return random.choice((True, False))
+
