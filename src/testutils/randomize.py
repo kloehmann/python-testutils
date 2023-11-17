c6cfe22f9485580de0bdff0c9ac9030e7271599a
Kevin Löhmann <{ID}+{username}@users.noreply.github.com>
2023-11-17 14:53:36 +0100

    added iter randomizer and renamed random module to avoid naming collisions

--- src/testutils/randomize.py
+++ src/testutils/randomize.py
@@ -3,3 +3,7 @@ import random, string
 
 def random_string(length):
     return "".join(random.choice(string.ascii_letters) for i in range(length))
+
+def random_iterator(length, elem_creator, *args):
+    return iter([ elem_creator(*args) for i in range(length) ] )
+
