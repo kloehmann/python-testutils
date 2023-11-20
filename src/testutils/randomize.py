44789445bbe36e7c07a4484656d668c5bc5fbc49
Kevin Löhmann <{ID}+{username}@users.noreply.github.com>
2023-11-20 16:23:46 +0100

    added list generator

--- src/testutils/randomize.py
+++ src/testutils/randomize.py
@@ -4,9 +4,14 @@ import random, string
 def random_string(length):
     return "".join(random.choice(string.ascii_letters) for i in range(length))
 
+
 def random_iterator(length, elem_creator, *args):
-    return iter([ elem_creator(*args) for i in range(length) ] )
+    return iter(random_list(length, elem_creator, *args))
+
+
+def random_list(length, elem_creator, *args):
+    return [elem_creator(*args) for i in range(length)]
+
 
 def random_bool():
     return random.choice((True, False))
-
